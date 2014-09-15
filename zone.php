1a948a890cfdd98061d3c543eb861877004cc2ab
scotchfield <swrittenb@gmail.com>
2014-09-14 22:09:10 -0400

    Retrieve zone, decode json, show basic info on zone action.

--- zone.php
+++ zone.php
@@ -1,33 +1,28 @@
 <?php
 
+function ts_get_zone( $zone_id ) {
+    $zone = get_zone( $zone_id );
+
+    if ( FALSE == $zone ) {
+        return;
+    }
+
+    $meta = json_decode( $zone[ 'meta_value' ], $assoc = TRUE );
+    foreach ( $meta as $k => $v ) {
+        $zone[ $k ] = $v;
+    }
+    unset( $zone[ 'meta_value' ] );
+
+    return $zone;
+}
+
 function ts_zone_set() {
     global $character;
 
     if ( strcmp( 'zone', game_get_action() ) ) {
         return;
     }
-
-// todo: think about how to handle zone assignment in twelve sands
-
-/*    if ( ! isset( $_GET[ 'zone_id' ] ) || ! isset( $_GET[ 'zone_id' ] ) ) {
-        return;
-    }
-
-    $zone = get_zone_by_tag( $_GET[ 'zone_tag' ] );
-    if ( FALSE != $zone ) {
-        return;
-    }*/
-
-/*    if ( '' != character_meta( ts_meta_type_character, TS_CURRENT_ZONE ) ) {
-        update_character_meta( $character[ 'id' ], ts_meta_type_character,
-            TS_CURRENT_ZONE, $zone_id );
-    } else {
-        add_character_meta( $character[ 'id' ], ts_meta_type_character,
-            TS_CURRENT_ZONE, $zone_id );
-    }
-
-    $character[ 'meta' ][ ts_meta_type_character ][ TS_CURRENT_ZONE ] =
-        $zone_id;*/
+//todo: need to do this?
 }
 
 add_action( 'action_set', 'ts_zone_set' );
@@ -38,6 +33,31 @@ function ts_zone_content() {
     if ( strcmp( 'zone', game_get_action() ) ) {
        return;
     }
+
+//todo: allow converting strings to numeric (i.e. erebus to 93928340)
+    $zone_id = 1; //todo: define as constant TS_STARTING_ZONE
+    if ( isset( $_GET[ 'zone_id' ] ) ) {
+        $zone_id = $_GET[ 'zone_id' ];
+    }
+
+    if ( ! is_numeric( $zone_id ) ) {
+        $zone_id = ts_str_to_int( $zone_id );
+        debug_print( 'zone_id changed to ' . $zone_id );
+    }
+
+    $zone = ts_get_zone( $zone_id );
+
+    if ( FALSE == $zone ) {
+        return;
+    }
+
+    debug_print( $zone );
+?>
+<div class="row">
+  <h3><?php echo( $zone[ 'name' ] ); ?></h3>
+  <h4><?php echo( $zone[ 'description' ] ); ?></h4>
+</div>
+<?php
 
 /*    $zone_id = GAME_STARTING_ZONE;
     if ( '' != character_meta( ts_meta_type_character, TS_CURRENT_ZONE ) ) {
@@ -97,3 +117,8 @@ function ts_zone_content() {
 }
 
 add_action( 'do_page_content', 'ts_zone_content' );
+
+function ts_str_to_int( $st ) {
+    $x = md5( $st );
+    return base_convert( $x, 16, 10 ) & 0xffffffff;
+}
