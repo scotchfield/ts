168651a1cad3bafb0cea80eefdcf8714250741fc
scotchfield <swrittenb@gmail.com>
2014-12-13 12:45:38 -0500

    Update unit test

--- test/tests/test_custom_core.php
+++ test/tests/test_custom_core.php
@@ -6,7 +6,7 @@ class TestTSCustomCore extends PHPUnit_Framework_TestCase {
      * @covers TwelveSands::__construct
      */
     public function test_ts_new() {
-        $ts = new TwelveSands();
+        $ts = new TwelveSands( array() );
 
         $this->assertNotNull( $ts );
     }
