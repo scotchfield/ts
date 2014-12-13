fd826722cfe2bbe3f9077ac9893bf074ab8cc802
scotchfield <swrittenb@gmail.com>
2014-12-13 12:39:08 -0500

    More code moving to the new object

--- custom-core.php
+++ custom-core.php
@@ -12,46 +12,58 @@ require( GAME_CUSTOM_PATH . 'zone.php' );
 
 class TwelveSands {
 
+    public $ag;
+
     public function __construct() {
+        global $ag;
+        $this->ag = $ag;
+
+        add_state( 'do_page_content', 'about', array( $this, 'about' ) );
+        add_state( 'do_page_content', 'contact', array( $this, 'contact' ) );
         add_state_priority( 'do_page_content', FALSE,
-                            array( $this, 'tip_print' ) );
+            array( $this, 'tip_print' ) );
+
+        add_state( 'arcadia_end', FALSE, array( $this, 'pack_character' ) );
+        add_state( 'award_achievement', FALSE,
+            array( $this, 'achievement_print' ) );
+        add_state_priority( 'character_load', FALSE,
+            array( $this, 'unpack_character' ) );
+        add_state( 'character_load', FALSE, array( $this, 'regen_stamina' ) );
+        add_state( 'game_header', FALSE, array( $this, 'header' ) );
+        add_state( 'game_footer', FALSE, array( $this, 'footer' ) );
         add_state( 'post_load', FALSE, array( $this, 'post_load' ) );
         add_state( 'select_character', FALSE, array( $this, 'login' ) );
         add_state( 'set_default_state', FALSE,
-                   array( $this, 'default_state' ) );
+            array( $this, 'default_state' ) );
+        add_state( 'validate_user', FALSE, array( $this, 'validate_user' ) );
+
     }
 
     public function post_load() {
-        global $ag;
-
-        $ag->set_component( 'achievement', new ArcadiaAchievement() );
-        $ag->set_component( 'inventory', new ArcadiaInventory() );
-        $ag->set_component( 'item', new ArcadiaItem() );
-        $ag->set_component( 'npc', new ArcadiaNpc() );
-        $ag->set_component( 'track_npc',
+        $this->ag->set_component( 'achievement', new ArcadiaAchievement() );
+        $this->ag->set_component( 'inventory', new ArcadiaInventory() );
+        $this->ag->set_component( 'item', new ArcadiaItem() );
+        $this->ag->set_component( 'npc', new ArcadiaNpc() );
+        $this->ag->set_component( 'track_npc',
             new ArcadiaTracking( $key_type = TRACK_NPC ) );
-        $ag->set_component( 'zone', new ArcadiaZone() );
+        $this->ag->set_component( 'zone', new ArcadiaZone() );
 
-        $ag->set_component( 'dashboard', new TSDashboard() );
+        $this->ag->set_component( 'dashboard', new TSDashboard() );
     }
 
     public function default_state() {
-        global $ag;
-
-        if ( FALSE == $ag->user ) {
-            $ag->set_state( 'title' );
-        } else if ( FALSE == $ag->char ) {
-            $ag->set_state( 'select' );
+        if ( FALSE == $this->ag->user ) {
+            $this->ag->set_state( 'title' );
+        } else if ( FALSE == $this->ag->char ) {
+            $this->ag->set_state( 'select' );
         } else {
-            $ag->set_state( 'zone' );
+            $this->ag->set_state( 'zone' );
         }
     }
 
     public function login() {
-        global $ag;
-
         ensure_character_meta_keygroup(
-            $ag->char[ 'id' ], ts_meta_type_character, '',
+            $this->ag->char[ 'id' ], ts_meta_type_character, '',
             array(
                 TS_INFO, TS_EQUIPPED, TS_ENCOUNTER,
                 TS_TIP,
@@ -59,9 +71,7 @@ class TwelveSands {
     }
 
     public function tip_print() {
-        global $ag;
-
-        if ( FALSE == $ag->char ) {
+        if ( FALSE == $this->ag->char ) {
             return;
         }
 
@@ -69,172 +79,146 @@ class TwelveSands {
 
         if ( 0 < strlen( $tip ) ) {
             echo( '<p class="tip">' . $tip . '</p>' );
-            update_character_meta( $ag->char[ 'id' ], ts_meta_type_character,
-                TS_TIP, '' );
+            update_character_meta( $this->ag->char[ 'id' ],
+                ts_meta_type_character, TS_TIP, '' );
         }
     }
 
-}
+    public function unpack_character() {
+        global $ag;
 
-
-define( 'ts_meta_type_character',            1 );
-define( 'ts_meta_type_buff',                 3 );
-
-define( 'TS_INFO', 1 );
-define( 'TS_EQUIPPED', 2 );
-define( 'TS_ENCOUNTER', 3 );
-
-define( 'TS_TIP', 10 );
-
-define( 'TRACK_NPC', 2000 );
-
-//define( 'CR_TUTORIAL_STATUS',                1 );
-
-
-
-function ts_unpack_character() {
-    global $ag;
-
-    if ( FALSE == $ag->char ) {
-        return;
-    }
-
-    $ag->char[ 'info' ] = json_decode(
-        character_meta( ts_meta_type_character, TS_INFO ), TRUE );
-
-    $default_info_obj = array(
-        'level' => 1,
-        'str' => 10,
-        'dex' => 10,
-        'int' => 10,
-        'con' => 10,
-        'cha' => 10,
-        'pow' => 10,
-        'health' => 10,
-        'health_max' => 10,
-        'mana' => 10,
-        'mana_max' => 10,
-        'stamina' => 100,
-        'stamina_max' => 100,
-        'stamina_timestamp' => 0,
-        'sanity' => 100,
-        'sanity_max' => 100,
-        'fatigue' => 0,
-        'fatigue_reduction' => 0,
-        'fatigue_rested' => 0,
-        'xp' => 0,
-        'gold' => 0,
-        'gold_bank' => 0,
-        'zone' => 1,
-        'burden' => 0,
-        'burden_max' => 100,
-    );
-
-    foreach ( $default_info_obj as $k => $v ) {
-        if ( ! isset( $ag->char[ 'info' ][ $k ] ) ) {
-            $ag->char[ 'info' ][ $k ] = $v;
+        if ( FALSE == $ag->char ) {
+            return;
         }
-    }
 
-    $ag->char[ 'equipped' ] = json_decode(
-        character_meta( ts_meta_type_character, TS_EQUIPPED ), TRUE );
+        $ag->char[ 'info' ] = json_decode(
+            character_meta( ts_meta_type_character, TS_INFO ), TRUE );
 
-    $default_info = array(
-      'weapon' => 0,
-      'head' => 0,
-      'chest' => 0,
-      'legs' => 0,
-      'neck' => 0,
-      'trinket' => 0,
-      'hands' => 0,
-      'wrists' => 0,
-      'belt' => 0,
-      'boots' => 0,
-      'ring' => 0,
-      'mount' => 0,
-    );
+        $default_info_obj = array(
+            'level' => 1,
+            'str' => 10,
+            'dex' => 10,
+            'int' => 10,
+            'con' => 10,
+            'cha' => 10,
+            'pow' => 10,
+            'health' => 10,
+            'health_max' => 10,
+            'mana' => 10,
+            'mana_max' => 10,
+            'stamina' => 100,
+            'stamina_max' => 100,
+            'stamina_timestamp' => 0,
+            'sanity' => 100,
+            'sanity_max' => 100,
+            'fatigue' => 0,
+            'fatigue_reduction' => 0,
+            'fatigue_rested' => 0,
+            'xp' => 0,
+            'gold' => 0,
+            'gold_bank' => 0,
+            'zone' => 1,
+            'burden' => 0,
+            'burden_max' => 100,
+        );
 
-    $default_cache = array();
-
-    foreach ( $default_info as $k => $v ) {
-        if ( ! isset( $ag->char[ 'equipped' ][ $k ] ) ) {
-            if ( ! isset( $default_cache[ $v ] ) ) {
-                $default_cache[ $v ] = $ag->c( 'item' )->get_item( $v );
+        foreach ( $default_info_obj as $k => $v ) {
+            if ( ! isset( $ag->char[ 'info' ][ $k ] ) ) {
+                $ag->char[ 'info' ][ $k ] = $v;
             }
+        }
 
-            $ag->char[ 'equipped' ][ $k ] = json_decode(
-                $default_cache[ $v ][ 'meta_value' ], TRUE );
+        $ag->char[ 'equipped' ] = json_decode(
+            character_meta( ts_meta_type_character, TS_EQUIPPED ), TRUE );
+
+        $default_info = array(
+          'weapon' => 0,
+          'head' => 0,
+          'chest' => 0,
+          'legs' => 0,
+          'neck' => 0,
+          'trinket' => 0,
+          'hands' => 0,
+          'wrists' => 0,
+          'belt' => 0,
+          'boots' => 0,
+          'ring' => 0,
+          'mount' => 0,
+        );
+
+        $default_cache = array();
+
+        foreach ( $default_info as $k => $v ) {
+            if ( ! isset( $ag->char[ 'equipped' ][ $k ] ) ) {
+                if ( ! isset( $default_cache[ $v ] ) ) {
+                    $default_cache[ $v ] = $ag->c( 'item' )->get_item( $v );
+                }
+
+                $ag->char[ 'equipped' ][ $k ] = json_decode(
+                    $default_cache[ $v ][ 'meta_value' ], TRUE );
+            }
+        }
+
+        $ag->char[ 'encounter' ] = json_decode(
+            character_meta( ts_meta_type_character, TS_ENCOUNTER ), TRUE );
+    }
+
+    public function pack_character() {
+        global $ag;
+
+        if ( FALSE == $ag->char ) {
+            return;
+        }
+
+        // todo: check if we need to update, don't just update every time
+
+        if ( isset( $ag->char[ 'info' ] ) ) {
+            update_character_meta( $ag->char[ 'id' ],
+                ts_meta_type_character, TS_INFO,
+                json_encode( $ag->char[ 'info' ] ) );
+        }
+
+        if ( isset( $ag->char[ 'equipped' ] ) ) {
+            update_character_meta( $ag->char[ 'id' ],
+                ts_meta_type_character, TS_EQUIPPED,
+                json_encode( $ag->char[ 'equipped' ] ) );
+        }
+
+        if ( isset( $ag->char[ 'encounter' ] ) ) {
+            update_character_meta( $ag->char[ 'id' ],
+                ts_meta_type_character, TS_ENCOUNTER,
+                json_encode( $ag->char[ 'encounter' ] ) );
         }
     }
 
-    $ag->char[ 'encounter' ] = json_decode(
-        character_meta( ts_meta_type_character, TS_ENCOUNTER ), TRUE );
-}
+    public function regen_stamina() {
+        global $ag;
 
-add_state_priority( 'character_load', FALSE, 'ts_unpack_character' );
+        if ( FALSE == $ag->char ) {
+            return;
+        }
 
-function ts_pack_character() {
-    global $ag;
+        if ( $ag->char[ 'info' ][ 'stamina' ] < 100 ) {
+            $stamina_boost = 1.0;
 
-    if ( FALSE == $ag->char ) {
-        return;
+            $stamina_seconds = time() -
+                $ag->char[ 'info' ][ 'stamina_timestamp' ];
+            $stamina_gain = $stamina_boost * ( $stamina_seconds / 120.0 );
+
+            $new_stamina = min(
+                100, $ag->char[ 'info' ][ 'stamina' ] + $stamina_gain );
+
+            $ag->char[ 'info' ][ 'stamina' ] = $new_stamina;
+            $ag->char[ 'info' ][ 'stamina_timestamp' ] = time();
+        }
     }
 
-    // todo: check if we need to update, don't just update every time
+    public function header() {
+        global $ag;
 
-    if ( isset( $ag->char[ 'info' ] ) ) {
-        update_character_meta( $ag->char[ 'id' ],
-            ts_meta_type_character, TS_INFO,
-            json_encode( $ag->char[ 'info' ] ) );
-    }
-
-    if ( isset( $ag->char[ 'equipped' ] ) ) {
-        update_character_meta( $ag->char[ 'id' ],
-            ts_meta_type_character, TS_EQUIPPED,
-            json_encode( $ag->char[ 'equipped' ] ) );
-    }
-
-    if ( isset( $ag->char[ 'encounter' ] ) ) {
-        update_character_meta( $ag->char[ 'id' ],
-            ts_meta_type_character, TS_ENCOUNTER,
-            json_encode( $ag->char[ 'encounter' ] ) );
-    }
-}
-
-add_state( 'arcadia_end', FALSE, 'ts_pack_character' );
-
-function ts_regen_stamina() {
-    global $ag;
-
-    if ( FALSE == $ag->char ) {
-        return;
-    }
-
-    if ( $ag->char[ 'info' ][ 'stamina' ] < 100 ) {
-        $stamina_boost = 1.0;
-
-        $stamina_seconds = time() - $ag->char[ 'info' ][ 'stamina_timestamp' ];
-        $stamina_gain = $stamina_boost * ( $stamina_seconds / 120.0 );
-
-        $new_stamina = min(
-            100, $ag->char[ 'info' ][ 'stamina' ] + $stamina_gain );
-
-        $ag->char[ 'info' ][ 'stamina' ] = $new_stamina;
-        $ag->char[ 'info' ][ 'stamina_timestamp' ] = time();
-    }
-}
-
-add_state( 'character_load', FALSE, 'ts_regen_stamina' );
-
-function ts_header() {
-    global $ag;
-
-    if ( ! strcmp( 'title', $ag->get_state() ) ) {
-        return;
-    }
-
-//    ts_unpack_character();
-//    update_buffs();
+        if ( ! strcmp( 'title', $ag->get_state() ) ) {
+            return;
+        }
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -269,8 +253,7 @@ function ts_header() {
               echo( GAME_NAME ); ?></a>
         </div>
 <?php
-
-    if ( FALSE != $ag->char ) {
+        if ( FALSE != $ag->char ) {
 ?>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
@@ -336,21 +319,21 @@ function ts_header() {
           </ul>
         </div>
 <?php
-    }
+        }
 ?>
       </div>
     </div>
 
     <div class="container">
 <?php
-}
-
-function ts_footer() {
-    global $ag;
-
-    if ( ! strcmp( 'title', $ag->get_state() ) ) {
-        return;
     }
+
+    public function footer() {
+        global $ag;
+
+        if ( ! strcmp( 'title', $ag->get_state() ) ) {
+            return;
+        }
 
 ?>
   </div>
@@ -363,19 +346,9 @@ function ts_footer() {
   </body>
 </html>
 <?php
-}
-
-add_state( 'game_header', FALSE, 'ts_header' );
-add_state( 'game_footer', FALSE, 'ts_footer' );
-
-
-function ts_about() {
-    global $ag;
-
-    if ( strcmp( 'about', $ag->get_state() ) ) {
-       return;
     }
 
+    public function about() {
 ?>
 <div class="row text-right">
   <h1 class="page_section">About</h1>
@@ -390,26 +363,60 @@ function ts_about() {
     Check out the project, and feel free to follow along as it grows!</p>
 </div>
 <?php
-}
-
-function ts_contact() {
-    global $ag;
-
-    if ( strcmp( 'contact', $ag->get_state() ) ) {
-       return;
     }
 
+    public function contact() {
 ?>
 <div class="row text-right">
   <h1 class="page_section">Contact</h1>
 </div>
 <?php
 
-    echo '<h1>OH BOB SAGET</h1>';
+        echo '<h1>Contact</h1>';
+    }
+
+    public function validate_user( $args ) {
+        if ( ! isset( $args[ 'user_id' ] ) ) {
+            return;
+        }
+
+        set_user_max_characters( $args[ 'user_id' ], 1 );
+    }
+
+    public function achievement_print( $args ) {
+        if ( ! isset( $args[ 'achievement_id' ] ) ) {
+            return;
+        }
+
+        $achievement = $this->ag->c( 'achievement' )->get_achievement(
+            $args[ 'achievement_id' ] );
+        $meta = json_decode( $achievement[ 'meta_value' ], TRUE );
+?>
+<div class="row text-center alert">
+  <h2>You have completed a new achievement!</h2>
+  <h3><?php echo( $meta[ 'name' ] ); ?></h3>
+  <h4><?php echo( $meta[ 'text' ] ); ?></h4>
+</div>
+<?php
+    }
+
 }
 
-add_state( 'do_page_content', FALSE, 'ts_about' );
-add_state( 'do_page_content', FALSE, 'ts_contact' );
+
+define( 'ts_meta_type_character',            1 );
+define( 'ts_meta_type_buff',                 3 );
+
+define( 'TS_INFO', 1 );
+define( 'TS_EQUIPPED', 2 );
+define( 'TS_ENCOUNTER', 3 );
+
+define( 'TS_TIP', 10 );
+
+define( 'TRACK_NPC', 2000 );
+
+//define( 'CR_TUTORIAL_STATUS',                1 );
+
+
 
 
 function ts_item_popup_str( $item ) {
@@ -428,37 +435,6 @@ function ts_item_string( $item ) {
            '\')" onmouseout="popout()" class="item">' . $item[ 'name' ] .
            '</a>';
 }
-
-function ts_validate_user( $args ) {
-    if ( ! isset( $args[ 'user_id' ] ) ) {
-        return;
-    }
-
-    set_user_max_characters( $args[ 'user_id' ], 1 );
-}
-
-add_state( 'validate_user', FALSE, 'ts_validate_user' );
-
-function ts_achievement_print( $args ) {
-    global $ag;
-
-    if ( ! isset( $args[ 'achievement_id' ] ) ) {
-        return;
-    }
-
-    $achievement = $ag->c( 'achievement' )->get_achievement(
-        $args[ 'achievement_id' ] );
-    $meta = json_decode( $achievement[ 'meta_value' ], TRUE );
-?>
-<div class="row text-center alert">
-  <h2>You have completed a new achievement!</h2>
-  <h3><?php echo( $meta[ 'name' ] ); ?></h3>
-  <h4><?php echo( $meta[ 'text' ] ); ?></h4>
-</div>
-<?php
-}
-
-add_state( 'award_achievement', FALSE, 'ts_achievement_print' );
 
 function ts_item_popup( $item ) {
     if ( ! isset( $item[ 'rarity' ] ) ) {
@@ -549,5 +525,7 @@ function ts_expand_id_obj( $item_obj ) {
 }
 
 // todo: attach this to $ag instead of new global
+// todo: simplify custom-core so that it just attached a twelvesands game
+//  object to $ag, then move the twelvesands game object to a new file
 global $ts;
 $ts = new TwelveSands();
