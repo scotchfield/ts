a2d9b1fa6f12f6cdb161e1b8bacaf3655a24f9b7
scotchfield <swrittenb@gmail.com>
2014-12-14 21:19:59 -0500

    A few more basic unit tests for the main ts object

--- test/tests/test_twelvesands.php
+++ test/tests/test_twelvesands.php
@@ -11,4 +11,57 @@ class TestTwelveSands extends PHPUnit_Framework_TestCase {
         $this->assertNotNull( $ts );
     }
 
+    /**
+     * @covers TwelveSands::post_load
+     */
+    public function test_ts_post_load() {
+        $ag = new ArcadiaGame();
+        $ts = new TwelveSands( $ag );
+
+        $ts->post_load();
+
+        $this->assertNotFalse( $ag->c( 'dashboard' ) );
+    }
+
+    /**
+     * @covers TwelveSands::default_state
+     */
+    public function test_ts_default_state_no_user() {
+        $ag = new ArcadiaGame();
+        $ts = new TwelveSands( $ag );
+
+        $ts->default_state();
+
+        $this->assertEquals( $ag->get_state(), 'title' );
+    }
+
+    /**
+     * @covers TwelveSands::default_state
+     */
+    public function test_ts_default_state_no_char() {
+        $ag = new ArcadiaGame();
+        $ts = new TwelveSands( $ag );
+
+        $ag->user = TRUE;
+
+        $ts->default_state();
+
+        $this->assertEquals( $ag->get_state(), 'select' );
+    }
+
+    /**
+     * @covers TwelveSands::default_state
+     */
+    public function test_ts_default_state_user_and_char() {
+        $ag = new ArcadiaGame();
+        $ts = new TwelveSands( $ag );
+
+        $ag->user = TRUE;
+        $ag->char = TRUE;
+
+        $ts->default_state();
+
+        $this->assertEquals( $ag->get_state(), 'zone' );
+    }
+
 }
