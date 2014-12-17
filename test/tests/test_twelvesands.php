f452756f0013bbdf5a11c53d6937cf78cd512a58
scotchfield <swrittenb@gmail.com>
2014-12-17 13:17:36 -0500

    Test should pass a valid ArcadiaGame object for add_state

--- test/tests/test_twelvesands.php
+++ test/tests/test_twelvesands.php
@@ -6,7 +6,8 @@ class TestTwelveSands extends PHPUnit_Framework_TestCase {
      * @covers TwelveSands::__construct
      */
     public function test_ts_new() {
-        $ts = new TwelveSands( array() );
+        $ag = new ArcadiaGame();
+        $ts = new TwelveSands( $ag );
 
         $this->assertNotNull( $ts );
     }
